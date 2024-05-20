4312f31b5c21f707f1a0fa516d5f7ac02a6772df
khoavn02 <khoavungoc02@gmail.com>
2024-05-20 23:08:00 +0700

    add DatabaseHelper

---
+++ app/src/main/java/com/android/fuze_music_player/database/DatabaseHelper.java
@@ -0,0 +1,58 @@
+package com.android.fuze_music_player.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "fuze_music";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create songs table
+        String create_songs_table = String.format(
+                "CREATE TABLE IF NOT EXISTS %s (%s TEXT PRIMARY KEY, %s TEXT, %s TEXT, %s TEXT, %s INTEGER, %s TEXT, %s TEXT)",
+                SongTable.TABLE_NAME,
+                SongTable.KEY_ID,
+                SongTable.KEY_TITLE,
+                SongTable.KEY_ARTIST,
+                SongTable.KEY_ALBUM,
+                SongTable.KEY_DURATION,
+                SongTable.KEY_IMG_URL,
+                SongTable.KEY_PATH
+        );
+        db.execSQL(create_songs_table);
+
+        // Create song history table
+        String create_song_history_table = String.format(
+                "CREATE TABLE IF NOT EXISTS %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT, %s INTEGER, FOREIGN KEY (%s) REFERENCES %s(%s))",
+                SongHistoryTable.TABLE_NAME,
+                SongHistoryTable.KEY_ID,
+                SongHistoryTable.KEY_SONG_ID,
+                SongHistoryTable.KEY_LAST_PLAYED_AT,
+                SongHistoryTable.KEY_SONG_ID,
+                SongTable.TABLE_NAME,
+                SongTable.KEY_ID
+        );
+        db.execSQL(create_song_history_table);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop songs table if exists
+        String drop_songs_table = String.format("DROP TABLE IF EXISTS %s", SongTable.TABLE_NAME);
+        db.execSQL(drop_songs_table);
+
+        // Drop song history table if exists
+        String drop_song_history_table = String.format("DROP TABLE IF EXISTS %s", SongHistoryTable.TABLE_NAME);
+        db.execSQL(drop_song_history_table);
+
+        // Recreate tables
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/com/android/fuze_music_player/database/SongHistoryTable.java
@@ -0,0 +1,9 @@
+package com.android.fuze_music_player.database;
+
+public class SongHistoryTable {
+    public static final String TABLE_NAME = "song_history";
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_SONG_ID = "song_id";
+    public static final String KEY_LAST_PLAYED_AT = "last_played_at";
+}
