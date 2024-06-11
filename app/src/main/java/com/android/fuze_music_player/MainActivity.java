da04da7e3287366a2b5e996979d1048737443f5a
thanhdacntt4 <teampkuminh@gmail.com>
2024-06-12 01:50:30 +0700

    SongandAlbum

--- app/src/main/java/com/android/fuze_music_player/MainActivity.java
+++ app/src/main/java/com/android/fuze_music_player/MainActivity.java
@@ -1,77 +1,52 @@
 package com.android.fuze_music_player;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
-
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.android.fuze_music_player.database.DatabaseHelper;
-import com.android.fuze_music_player.model.AlbumModel;
-import com.android.fuze_music_player.service.AlbumService;
-import com.android.fuze_music_player.service.IAlbumService;
-import com.android.fuze_music_player.service.ISongService;
-import com.android.fuze_music_player.service.SongService;
-
-import java.util.ArrayList;
-import java.util.List;
+import androidx.viewpager2.widget.ViewPager2;
+import com.android.fuze_music_player.ViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button btnSelectDir;
-    private ISongService songService;
-
-    private IAlbumService albumService;
-
-    private DatabaseHelper databaseHelper;
-    private ActivityResultLauncher<Intent> pickSongsLauncher;
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
-        databaseHelper = new DatabaseHelper(this);
-        songService = new SongService(databaseHelper);
-        albumService = new AlbumService(databaseHelper);
-        setupActivityResultLauncher();
-        List<AlbumModel> albumModels = albumService.list();
+
+        // Khởi tạo ViewPager2 và TabLayout
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.view_pager);
+
+        // Set up ViewPager2 và adapter
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager2.setAdapter(viewPagerAdapter);
+
+        // Liên kết TabLayout và ViewPager2
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
     }
 
-    private void init() {
-        this.btnSelectDir = findViewById(R.id.btnSelectDir);
-        this.btnSelectDir.setOnClickListener(v -> openFilePicker());
-    }
-
-    private void setupActivityResultLauncher() {
-        pickSongsLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == RESULT_OK) {
-                        Intent data = result.getData();
-                        if (data != null) {
-                            List<Uri> uris = new ArrayList<>();
-                            if (data.getClipData() != null) {
-                                for (int i = 0; i < data.getClipData().getItemCount(); i++) {
-                                    uris.add(data.getClipData().getItemAt(i).getUri());
-                                }
-                            } else if (data.getData() != null) {
-                                uris.add(data.getData());
-                            }
-                            songService.importAudioFromUris(this, uris);
-                        }
-                    }
-                });
-    }
-
-    private void openFilePicker() {
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.setType("audio/*");
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        pickSongsLauncher.launch(intent);
-    }
 }
--- app/src/main/java/com/android/fuze_music_player/model/AlbumModel.java
+++ app/src/main/java/com/android/fuze_music_player/model/AlbumModel.java
@@ -2,14 +2,29 @@ package com.android.fuze_music_player.model;
 
 public class AlbumModel {
 
+    private int id;
     private String name;
-
     private String imgUrl;
+    private String artist;
 
-
-    public AlbumModel(String name, String imgUrl) {
+    public AlbumModel(int id, String name, String imgUrl, String artist) {
+        this.id = id;
         this.name = name;
         this.imgUrl = imgUrl;
+        this.artist = artist;
+    }
+
+    public AlbumModel(String albumName, Object name) {
+    }
+
+    // Getters và Setters cho các trường
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -26,5 +41,13 @@ public class AlbumModel {
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
     }
 }
