2d44fc73dc972fdca3176a8d6a342e95e38ac84c
khoavn02 <khoavungoc02@gmail.com>
2024-06-25 13:21:12 +0700

    remove unused Database Helper

--- app/src/main/java/com/android/fuze_music_player/database/SongHistoryTable.java
+++
@@ -1,9 +0,0 @@
-package com.android.fuze_music_player.database;
-
-public class SongHistoryTable {
-    public static final String TABLE_NAME = "song_history";
-
-    public static final String KEY_ID = "id";
-    public static final String KEY_SONG_ID = "song_id";
-    public static final String KEY_LAST_PLAYED_AT = "last_played_at";
-}
